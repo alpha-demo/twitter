1448bd9b6369a546e23707aab147b390ac78f93f
Alejandro D. Bakker Antezana <alpha.demo.007@gmail.com>
2019-03-14 14:19:50 +0100

    Including the PHPStorm .idea directory to the .gitignore file.

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+/* //// ********************* /// ************** // ******* /              M O N A L Y S E              / ******* // ************** /// *********************  //// */
+
+/**
+ * Enterprise   MONALYSE
+ * Summary      MONALYSE + TechnicalTestMonalyse
+ * @author      Alejandro D. Bakker Antezana.
+ * @since       Wednesday, 14/03/2019
+ * @version     1.0.0 / PHP Version: 7.3.1
+ */
+
+    echo "MONALYSE + TechnicalTestMonalyse";
+
+
+/* //// ********************* /// ************** // ******* // O // ******* // ************** /// *********************  //// */
+
+
